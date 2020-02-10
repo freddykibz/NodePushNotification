8d974b4a0c9151708c01d0fe17047fb887a22f9b
freddykibz <fred@kibzagency.com>
2020-02-10 21:25:20 +0300

    fix

--- push.js
+++ push.js
@@ -6,7 +6,11 @@ let vapidkeys ={
 }
 
 push.setVapidDetails('mailto:fred@kibzagency.com', vapidkeys.publicKey, vapidkeys.privateKey)
-let sub ={};
+let sub ={
+  "endpoint":"https://fcm.googleapis.com/fcm/send/dwOqTEzeZmU:APA91bH6xOdBw_DdxfyXiAY4577wVN33xP3BWwDa4KakN_rIFOXxcb2k8Xh0XqkVlY4bNjk0bLmH4QtSq1cWXjCP-dhWQJiC0Mr_Lf-N2NJGasyEVCqIM_RplxABrKmqb_TVHBB-fvtp","expirationTime":null,"keys":{"p256dh":"BNZ7_z7TM3BI0G0bupefgjZbxpEqSDBteHHwPJGkJQssyIbBlBtjQmbk-Fb-zNpGocW-FzvrQzXh-19kAg2cI-c",
+  "auth":"ltJftFkLx9-qX5dU2WK78w"}
+
+};
 push.sendNotification(sub, 'test message')
 
 
